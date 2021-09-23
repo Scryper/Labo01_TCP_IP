52e16b2d8604882d66d8692cb5f79d40d7e1a65c
Dotni <57830936+Dotni@users.noreply.github.com>
2021-09-23 18:42:20 +0200

    1 function done

--- js/functionality_1.js
+++ js/functionality_1.js
@@ -3,4 +3,65 @@ var btn = document.getElementById('send-f1');
 var answerF1 = document.getElementById('answerF1');
 
 //when you click on the button
-btn.addEventListener('click', verifyIPAdress);
+btn.addEventListener('click', ipClass);
+
+//return the class of an IP adress
+function ipClass(){
+    //get the IP adress
+    let ip = document.getElementById('ip_adress-f1').value;
+
+    let ipPart = new Array(4);
+
+    if(verifyIPAdress(ip,ipPart)){
+        let classIP="";
+        let nbReseau=0;
+        let nbHote=0;
+
+        //Class A
+        if(ipPart[0]>0&&ipPart[0]<127){
+            classIP="A";
+            nbReseau=126;
+            nbHote=16777214;
+        }
+        else{
+            //Class B
+            if(ipPart[0]>=128&&ipPart[0]<192){
+                classIP="B";
+                nbReseau=16384;
+                nbHote=65534;
+            }
+            else{
+                //Class C
+                if(ipPart[0]>=192&&ipPart[0]<224){
+                    classIP="C";
+                    nbReseau=2097152;
+                    nbHote=254;
+                }
+                else{
+                    //Class D
+                    if(ipPart[0]>=224&&ipPart[0]<240){
+                        classIP="D";
+                    }
+                    else{
+                        //Class E
+                        if(ipPart[0]>=240&&ipPart[0]<248){
+                            classIP="E";
+                        }
+                        else{
+                            classIP="no class"
+                        }
+                    }
+                }
+            }
+        }
+
+        answerF1.innerText="Class : " + classIP +"\nNumber of subnetwork : "+nbReseau +"\nNumber of host : "+nbHote;
+    }
+    else{
+        answerF1.innerText="The IP adress entered is incorrect."
+    }
+}
+
+
+
+
--- js/ipVerificator.js
+++ js/ipVerificator.js
@@ -1,6 +1,4 @@
-var funtionizi = function verifyIPAdress() {
-    //get the IP adress
-    let ip = document.getElementById('ip_adress-f1').value;
+function verifyIPAdress(ip,ipPart) {
 
     //check with a regex if the input is correct
     let regex = "^([0-9]{1,3}\\.){3}[0-9]{1,3}$";
@@ -9,10 +7,9 @@ var funtionizi = function verifyIPAdress() {
     //if the regex is ok
     if(regexObj.test(ip)){
 
-        /* remove the "."'s and divide the IP adress into 4 parts */
-        //define an array
-        let ipPart = new Array(4);
+        let incorret = false;
 
+        /* remove the "."'s and divide the IP adress into 4 parts */
         //select all characters before the point
         for (let i = 0; i < 3; i++) {
             ipPart[i] = ip.substring(0,ip.indexOf("."));
@@ -22,67 +19,16 @@ var funtionizi = function verifyIPAdress() {
         //what is left is the last part of the IP
         ipPart[3] = ip;
 
-        //boolean
-        let wrong=false;
-
         //verify if each number is between 0 and 255 included
-        for(let i = 0; i < 3; i++){
+        for(let i = 0; i < 4; i++){
             if(ipPart[i]<0||ipPart[i]>255){
-                wrong=true;
+                incorret=true;
             }
         }
-
-        let classIP="";
-        let nbReseau=0;
-        let nbHote=0;
-
-
-        //Class A
-        if(ipPart[0]>0&&ipPart[0]<127){
-            classIP="A";
-            nbReseau=126;
-            nbHote=16777214;
-        }
-        else{
-            //Class B
-            if(ipPart[0]>=128&&ipPart[0]<192){
-                classIP="B";
-                nbReseau=16384;
-                nbHote=65534;
-            }
-            else{
-                //Class C
-                if(ipPart[0]>=192&&ipPart[0]<224){
-                    classIP="C";
-                    nbReseau=2097152;
-                    nbHote=254;
-                }
-                else{
-                    //Class D
-                    if(ipPart[0]>=224&&ipPart[0]<240){
-                        classIP="D";
-                    }
-                    else{
-                        //Class E
-                        if(ipPart[0]>=240&&ipPart[0]<248){
-                            classIP="E";
-                        }
-                        else{
-                            wrong=true;
-                        }
-                    }
-                }
-            }
-        }
-
-
-        if(wrong==true){
-            console.log("IP incorrecte");
-        }
-        else{
-            console.log(classIP + " " + nbHote + " " + nbReseau);
-        }
+        if(!incorret)
+        return true;
     }
+    return false;
 }
 
 
